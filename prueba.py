110bd1253486be7e9ca7e2e473b5f1eabf7c21e2
LucasEzequielGiordano <lucasgiordano2219@gmail.com>
2022-09-16 17:30:49 -0300

    feat: imc calculator

--- prueba.py
+++ prueba.py
@@ -1,2 +1,5 @@
-saludar = 'Hola Mundo!'
-print(saludar)
+peso = int(input("¿cuál es tu peso? "))
+estatura = float(input("¿cuál es tu altura?"))
+imc = peso / (estatura  ** 2)
+
+print("Tu índice de masa corporal es: ", imc) 
