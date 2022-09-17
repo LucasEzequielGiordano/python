428cbe5e2a8f0fd41117ac835fcddbbc5cd6c5cb
LucasEzequielGiordano <lucasgiordano2219@gmail.com>
2022-09-17 17:35:26 -0300

    feat: leap year

--- main.py
+++ main.py
@@ -5,8 +5,19 @@
 # print("Tu índice de masa corporal es: ", imc) 
 
 # Orden al reves
-lista = []
-for numero in range(1, 101):
-    lista.append(numero)
-    ordenInverso = sorted(lista, reverse=True)
-print(ordenInverso)
+# lista = []
+# for numero in range(1, 101):
+#     lista.append(numero)
+#     ordenInverso = sorted(lista, reverse=True)
+# print(ordenInverso)
+
+# Funcion años bisiestos
+def bisiesto():
+    year = int(input('Ingresa un año para saber si es bisiesto: '))
+
+    if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+        return f'{year} es un año bisiesto'
+    else:
+        return f'{year} no es un año bisiesto'
+
+print(bisiesto())
