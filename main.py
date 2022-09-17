eb5c418b82afef6974d139680cc1c4d6019c2905
LucasEzequielGiordano <lucasgiordano2219@gmail.com>
2022-09-16 21:07:50 -0300

    feat: order numbers

---
+++ main.py
@@ -0,0 +1,12 @@
+# IMC
+# peso = int(input("¿cuál es tu peso? "))
+# estatura = float(input("¿cuál es tu altura?"))
+# imc = peso / (estatura  ** 2)
+# print("Tu índice de masa corporal es: ", imc) 
+
+# Orden al reves
+lista = []
+for numero in range(1, 101):
+    lista.append(numero)
+    ordenInverso = sorted(lista, reverse=True)
+print(ordenInverso)
--- prueba.py
+++
@@ -1,5 +0,0 @@
-peso = int(input("¿cuál es tu peso? "))
-estatura = float(input("¿cuál es tu altura?"))
-imc = peso / (estatura  ** 2)
-
-print("Tu índice de masa corporal es: ", imc) 
